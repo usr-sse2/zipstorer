68e7d184f6e3e82077840416f8ef01bce1316566
Michael Belyaev <usrsse2@me.com>
2019-02-21 18:11:03 +0300

    Bump version

--- net45/Properties/AssemblyInfo.cs
+++ net45/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Zipstorer")]
-[assembly: AssemblyCopyright("Copyright © Jaime Olivares 2017")]
+[assembly: AssemblyCopyright("Copyright © Jaime Olivares 2017, usrsse2 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.0")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
